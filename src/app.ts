5f47695f5b424175794713356e8f7413e0528b55
Mariángel Quiceno Largo <126704306+MariangelQuiceno@users.noreply.github.com>
2024-05-16 13:36:50 -0500

    conectar base de datos mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import { envs } from "./config";
+import { MongoDatabase } from "./data/mongodb/mongo-database"; //2
 import { AppRoutes } from "./presentation/router"; //1
 import { Server } from "./presentation/server";
 
-(()=> {
-    main();
-  })()
-  
-  async function main (){
-  // todo: await base de datos
-  
-  // todo: inicio de nuestro server
-      new Server({
-        port: envs.PORT,
-        routes: AppRoutes.routes //2
-      }).start();
-  }
+(() => {
+  main();
+})();
 
-  
+async function main() {
+  // todo: await base de datos
+  await MongoDatabase.connect({//1
+    //3
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  });
+  // todo: inicio de nuestro server
+  new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes, //2
+  }).start();
+}
